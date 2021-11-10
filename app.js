fc363340189efccedbdd36e313a5817f95a69f06
i-m-reddy <iamrishab7701@gmail.com>
2021-11-10 18:44:56 +0530

    week 3

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -20,15 +23,19 @@ const dishRouter = require('./routes/DishRouter.js');
 const leaderRouter = require('./routes/LeaderRouter');
 const promoRouter = require('./routes/PromoRouter');
 
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,6 +44,7 @@ app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/leaders',leaderRouter);
 app.use('/promotions',promoRouter)
+app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
